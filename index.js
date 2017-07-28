101de4ccb64a88a377fae2a520ef3872621a29ef
Venkata Thippanna Rao. P. R <venkat.iiitb@gmail.com>
2017-07-28 16:02:18 +0530

    push

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+var express = require('express')
+var app = express()
 
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-var http = require('http'),
-fs = require('fs');
+app.get('/', function(request, response) {
+response.send('Hello World!')
+})
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(8000);
-});
+app.listen(app.get('port'), function() {
+console.log("Node app is running at localhost:" + app.get('port'))
+})
